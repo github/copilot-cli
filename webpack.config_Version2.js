7104a3e4187b417adf8b694f4b07a3db4e0ad6cf
tijuks <154919533+tijuks@users.noreply.github.com>
2025-12-31 00:05:46 +0300

    Add files via upload

---
+++ webpack.config_Version2.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+};
